150ba3495f11d132914ab42d7500e9748d0733f3
Ha Nguyen <nguyenmha07092003@gmail.com>
2024-06-13 19:34:41 +0700

    fix error deploy

--- src/app/(main)/blog/page.tsx
+++ src/app/(main)/blog/page.tsx
@@ -5,12 +5,8 @@ import ListBlog from "./_component/ListBlog";
 const Blog = () => {
   return (
     <section className="container py-4 md:px-8 space-y-4 size-full">
-      <ListSubject />
       <div className="flex items-center justify-between">
         <h1 className="font-medium text-lg">Danh sách bài viết</h1>
-      </div>
-      <div className="min-h-96 h-full flex">
-        <ListBlog />
       </div>
     </section>
   );
--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -6,19 +6,35 @@ import http from "@/lib/http";
 import { ISlider } from "@/lib/interface";
 
 export default async function Home() {
-  const { payload: products } = await productApiRequest.handleSearchProduct({
-    limit: 20,
-    page: 1,
-    search: "",
-  });
-  const { payload: sliders } = await http.get<ISlider[]>("/admin/slider");
   return (
     <div className="flex container gap-4 py-4 flex-col md:flex-row">
       <Category />
       <div className="flex flex-col gap-4 flex-1">
-        <SliderEvent sliders={sliders} />
-        <ProductSuggest products={products} />
+        <Event />
+        <Products />
       </div>
     </div>
   );
 }
+
+const Event = async () => {
+  try {
+    const { payload: sliders } = await http.get<ISlider[]>("/admin/slider");
+    return <SliderEvent sliders={sliders} />;
+  } catch (error) {
+    return null;
+  }
+};
+
+const Products = async () => {
+  try {
+    const { payload: products } = await productApiRequest.handleSearchProduct({
+      limit: 20,
+      page: 1,
+      search: "",
+    });
+    return <ProductSuggest products={products} />;
+  } catch (error) {
+    return null;
+  }
+};
